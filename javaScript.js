a522f2c538a8277fd46117405976def6674e1650
jbdwwm48 <jb.charbonnier.dwwm48@gmail.com>
2024-12-19 14:47:37 +0100

    update JS Secret & lightbox

---
+++ javaScript.js
@@ -0,0 +1,43 @@
+// secret
+
+function displaySecret() {
+    var secret = document.getElementById("secret");
+    if (secret.style.display === "none") {
+        secret.style.display = "flex";
+    } else {
+        secret.style.display = "none";
+    }
+}
+
+// Lightbox
+
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
+
+const images = document.querySelectorAll('img');
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active');
+        const img = document.createElement('img');
+        img.src = image.src;
+        while (lightbox.firstChild) {
+        lightbox.removeChild(lightbox.firstChild);
+        }
+        lightbox.appendChild(img);
+    })
+})
+
+const closeButton = document.createElement ('button');
+    closeButton.textContent = 'X';
+    closeButton.className = 'button__close';
+    lightbox.appendChild(closeButton);
+
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return;
+        lightbox.classList.remove('active');
+})
+
+closeButton.addEventLinstener('click', () => {
+    lightbox.classList.remove('active');
+})
